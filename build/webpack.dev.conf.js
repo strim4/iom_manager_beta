851334dce816edd5264831566b9b22dcb6739a13
Strickler Moritz <moritz.strickler@dvbern.ch>
2020-01-06 20:34:41 +0100

    travis2

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -24,9 +24,26 @@ const devWebpackConfig = merge(baseWebpackConfig, {
   devServer: {
     clientLogLevel: 'warning',
     historyApiFallback: {
-      rewrites: [
-        { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath, 'index.html') },
-      ],
+    rewrites: [
+    { from: /.*/, to: path.posix.join(config.dev.assetsPublicPath,
+    'index.html') },
+    ],
+    },
+    hot: true,
+    contentBase: false, // since we use CopyWebpackPlugin.
+    compress: true,
+    host: HOST || config.dev.host,
+    port: PORT || config.dev.port,
+    open: config.dev.autoOpenBrowser,
+    overlay: config.dev.errorOverlay
+    ? { warnings: false, errors: true }
+    : false,
+    publicPath: config.dev.assetsPublicPath,
+    quiet: true, // necessary for FriendlyErrorsPlugin
+    watchOptions: {
+	poll: config.dev.poll,
+},
+
     },
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
