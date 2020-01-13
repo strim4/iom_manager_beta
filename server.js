72d6c5363fca7584b5dc9aff4f4947aaef8c045f
strim4 <moritz_strickler@msn.com>
2020-01-13 13:54:12 +0100

    asdf

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ function isLoggedIn(req, res, next) {
 };
 
 //Tell express server to use port 8081
-const port = process.env.API_PORT || 8081;
+const port = process.env.PORT || 8081;
 app.use('/', router);
 app.listen(port, function() {
 console.log(`api running on port ${port}`);
